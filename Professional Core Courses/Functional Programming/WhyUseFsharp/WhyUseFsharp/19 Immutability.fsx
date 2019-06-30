d58f8b29cea7632ab373e5d5a8524afb277e94a4
Medill-East <NEMailLHD@163.com>
2019-07-01 00:46:01 +0800

    why use f#
    update 201907010045

--- Professional Core Courses/Functional Programming/WhyUseFsharp/WhyUseFsharp/19 Immutability.fsx
+++ Professional Core Courses/Functional Programming/WhyUseFsharp/WhyUseFsharp/19 Immutability.fsx
@@ -1,1 +1,23 @@
-﻿
+﻿//immutable list
+let list = [1,2,3,4]
+
+type PersonalName = {FirstName:string; LastName:string}
+
+// immutable person
+let john = {FirstName = "John"; LastName = "Doe"}
+
+let alice = {john with FirstName = "Alice"}
+
+// create an immutable list
+let list1 = [1;2;3;4]
+
+// prepend to make a new list
+let list2 = 0::list1
+
+// get the last 4 of the second list
+let list3 = list2.Tail
+
+// the two lists are the identical object in memory
+System.Object.ReferenceEquals(list1,list3)
+
+
