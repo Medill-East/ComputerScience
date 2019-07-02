fbf1c7b7f15de4c845d9cb4517d0e9903075f856
Medill-East <NEMailLHD@163.com>
2019-07-03 00:07:12 +0800

    why use f# 23-24
    20190702

---
+++ Professional Core Courses/Functional Programming/WhyUseFsharp/WhyUseFsharp/24 Asynchronous programming.fsx
@@ -0,0 +1,10 @@
+﻿/// /// /// /// /// /// Traditional asynchronous programming
+open System
+
+let userTimerWithCallback = 
+    // create an event to wait on
+    let event = new System.Threading.AutoResetEvent(false)
+
+    // create a timer and add an event handler that will signal the event
+    let timer = new System.Timers.Timer(2000.0)
+    timer.Elapsed.Add()
