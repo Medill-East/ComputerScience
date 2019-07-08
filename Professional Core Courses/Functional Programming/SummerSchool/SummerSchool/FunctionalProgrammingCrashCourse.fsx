88ac60c541857fd1af4499779809b2cc3d886546
Medill-East <NEMailLHD@163.com>
2019-07-08 13:50:50 +0800

    Summer School - Functional Programming
    20190708 before noon

---
+++ Professional Core Courses/Functional Programming/SummerSchool/SummerSchool/FunctionalProgrammingCrashCourse.fsx
@@ -0,0 +1,10 @@
+﻿printf "%f" (3.14+2.78)
+let cal = 3.14 + 2.78
+printfn "%A" cal
+
+164uy + 230uy
+
+let fulllist = "hello world"
+let hello = fulllist.[..4]
+let world = fulllist.[6..]
+printfn "first: %A, second: %A" hello world
