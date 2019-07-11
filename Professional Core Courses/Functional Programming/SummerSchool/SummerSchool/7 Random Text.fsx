d916cad738aa4c7e10fe28429e949d051239623b
Medill-East <NEMailLHD@163.com>
2019-07-11 17:59:00 +0800

    change
    201907111758

--- Professional Core Courses/Functional Programming/SummerSchool/SummerSchool/7 Random Text.fsx
+++ Professional Core Courses/Functional Programming/SummerSchool/SummerSchool/7 Random Text.fsx
@@ -38,8 +38,8 @@ let rec convertText (src:string) : string =
             elm.[0].ToString() + (convertText elm.[1..])
         else
             convertText elm.[1..]
-            
-//printfn "convert %A to %A" testString (convertText testString)
+
+printfn "convert %A to %A" testString (convertText testString)
 
 //7.3
 let rec countchar (src:string)(letter:char): int list = 
@@ -115,7 +115,6 @@ let randomString (hist : int list) (len : int) : string =
 //let newHist = histogram ranStr
 //printfn "Resulting histogram:\n %A" (List.zip alphabet newHist)
 
-
 /// /// /// 7.4 step2 readfile
 let str = readText "littleClausAndBigClaus.txt"
 /// /// /// 7.4 step3 convertfile
@@ -129,3 +128,114 @@ let ranStr = randomString hist convertRes.Length
 printfn "A random string: %s" ranStr
 let newHist = histogram ranStr
 printfn "Resulting histogram:\n %A" (List.zip alphabet newHist)
+
+/// /// /// /// /// 7.5
+let rec countnum (src:string) (charpair:string):int =
+  if src.Length >=2 then
+    if src.[0..1] = charpair then
+      1+(countnum src.[1..] charpair)
+    else
+      (countnum src.[1..] charpair)
+  else
+    0
+let rec countcharpair (src:string) (letter1:char) (letter2:char): int list =
+  if letter2 <= 'z' then
+    let charpair = string letter1 + string letter2
+    (countnum src charpair) :: (countcharpair src letter1 (letter2 + char 1))
+  else
+    []
+
+let rec countchar (src:string) (letter1:char) : int list list = 
+  if letter1 <= 'z' then
+    (countcharpair src letter1 'a')::(countchar src (letter1 + char 1))
+  else
+    []
+let cooccurrence (src:string):int list list =
+  countchar src 'a'
+
+let storyContent = "“LITTLE CLAUS AND BIG CLAUS a translation of hans christian andersen’s ’lille claus og store claus’ by jean hersholt.
+
+In a village there lived two men who had the self-same name. Both were named Claus. But one of them owned four horses, and the other owned only one horse; so to distinguish between them people called the man who had four horses Big Claus, and the man who had only one horse Little Claus. Now I’ll tell you what happened to these two, for this is a true story.”
+"
+printfn "occurence of %A is %A" storyContent (cooccurrence storyContent)
+
+
+/// /// /// /// /// 7.6 fstOrderMarkovModel
+
+
+
+let fstOrderMarkovModel (cooc : int list list) (len:int) : string = 
+
+
+    /// /// /// 7.6 step1 get the occurrence of story
+  
+
+    /// /// /// 7.6 step2 generates random string of length len whose character pairs are distruibuted
+    /// /// /// according to occurrence of story
+
+    /// /// /// 7.6 compare the result of step1 and step2
+
+    
+
+
+    ///// Reverse the order of a list of any type
+    //let rec reverse (lst : 'a list list) : 'a list list =
+    //  match lst with
+    //    elm :: rest -> (reverse rest) @ [elm]
+    //    | [] -> []
+
+    ///// Calculate the cumulative sum of a list of integers from the first to the last element. First element is the first number in the original list, last element is the sum of all integers in the original list.
+    //let cumSum (lst : int list list) : int list =
+    //  /// Prepend the sum of the first element in acc and elm to acc.
+    //  let updateCumSum (acc : int list list) (elm : int) : int list =
+    //    match acc with
+    //      [] -> [elm]
+    //      | a -> a.Head + elm :: a
+
+    //  List.fold updateCumSum [] lst |> reverse
+
+    ///// Given a monotonic function and an index into its value set, find the corresponding value on its definition set.
+    //let reverseLookup (monotonic : 'a list) (v : 'a) : int =
+    //    try
+    //      List.findIndex (fun w -> w > v) monotonic
+    //    with
+    //      _ -> monotonic.Length - 1
+
+    ///// Generate a random character pair according to a histogram.
+    //let rnd = System.Random() // A global object, if included in randomChar then seed point only changes slowly giving strongly correlated results in time
+    //let randomCharPair (hist : int list list) : char =
+    //  let cumHist = cumSum hist
+    //  let v = rnd.Next(cumHist.[cumHist.Length-1])
+    //  let i = reverseLookup cumHist v
+    //  'a' + char i
+
+    ///// Generate a string of random character pairs each distributed according to a histogram.
+    //let randomString (hist : int list list) (len : int) : string = 
+      ///// Append random characters to a string, each distributed according to a histogram.
+      //let rec appendRandomString (hist : int list) (len : int) (src : string) : string = 
+      //  if len = 0 then
+      //    src
+      //  else
+      //    src + string (randomChar hist) |> appendRandomString hist (len-1)
+
+      //appendRandomString hist len ""
+
+
+
+
+    ///// /// /// 7.4 step2 readfile
+    //let str = readText "littleClausAndBigClaus.txt"
+    ///// /// /// 7.4 step3 convertfile
+    //let convertRes = convertText str
+    ///// /// /// 7.4 step4 get histogram
+    //let hist = histogram convertRes
+    //let alphabet = List.init hist.Length (fun i -> 'a' + char i)
+    //printfn "A histogram:\n %A" (List.zip alphabet hist)
+    ///// /// /// 7.4 step5 generages a new random string
+    //let ranStr = randomString hist convertRes.Length
+    //printfn "A random string: %s" ranStr
+    //let newHist = histogram ranStr
+    //printfn "Resulting histogram:\n %A" (List.zip alphabet newHist)   
+
+
+//printfn "random string of occurrence %A are %A" (cooccurrence storyContent) (fstOrderMarkovModel (cooccurrence storyContent) (storyContent.Length))
---
+++ Professional Core Courses/Functional Programming/SummerSchool/SummerSchool/7.6 fstOrderMarkovModel.fsx
@@ -0,0 +1,75 @@
+﻿
+//let fstOrderMarkovModel (cooc : int list list) (len:int) : string = 
+
+
+    /// /// /// 7.6 step1 get the occurrence of story
+  
+
+    /// /// /// 7.6 step2 generates random string of length len whose character pairs are distruibuted
+    /// /// /// according to occurrence of story
+
+    /// /// /// 7.6 compare the result of step1 and step2
+
+/// Reverse the order of a list of any type
+let rec reverse (lst : 'a list) : 'a list =
+  match lst with
+    elm :: rest -> (reverse rest) @ [elm]
+    | [] -> []
+
+/// Calculate the cumulative sum of a list of integers from the first to the last element. First element is the first number in the original list, last element is the sum of all integers in the original list.
+let cumSum (lst : int list list) : int list =
+  /// Prepend the sum of the first element in acc and elm to acc.
+  let updateCumSum (acc : int list) (elm : int) : int list =
+    match acc with
+      [] -> [elm]
+      | a -> a.Head + elm :: a
+
+  List.fold updateCumSum [] lst |> reverse
+
+/// Given a monotonic function and an index into its value set, find the corresponding value on its definition set.
+let reverseLookup (monotonic : 'a list) (v : 'a) : int =
+    try
+      List.findIndex (fun w -> w > v) monotonic
+    with
+      _ -> monotonic.Length - 1
+
+/// Generate a random character pair according to a histogram.
+let rnd = System.Random() // A global object, if included in randomChar then seed point only changes slowly giving strongly correlated results in time
+let randomCharPair (hist : int list list) : (char*char) =
+  let cumHist = cumSum hist
+  let v = rnd.Next(cumHist.[cumHist.Length-1])
+  let i = reverseLookup cumHist v
+  'a' + char i    
+
+
+
+/// Generate a string of random character pairs distributed according to a histogram.
+let randomStringPair (hist : int list list) (len : int) : string = 
+  /// Append random character pair to a string, each distributed according to a histogram.
+  let rec appendRandomStringPair (hist : int list list) (len : int) (src : string) : string = 
+    if len = 0 then
+      src
+    else
+      src + string (randomCharPair hist) |> appendRandomStringPair hist (len-1)
+
+  appendRandomStringPair hist len ""
+  
+
+    ///// /// /// 7.4 step2 readfile
+    //let str = readText "littleClausAndBigClaus.txt"
+    ///// /// /// 7.4 step3 convertfile
+    //let convertRes = convertText str
+    ///// /// /// 7.4 step4 get histogram
+    //let hist = histogram convertRes
+    //let alphabet = List.init hist.Length (fun i -> 'a' + char i)
+    //printfn "A histogram:\n %A" (List.zip alphabet hist)
+    ///// /// /// 7.4 step5 generages a new random string
+    //let ranStr = randomString hist convertRes.Length
+    //printfn "A random string: %s" ranStr
+    //let newHist = histogram ranStr
+    //printfn "Resulting histogram:\n %A" (List.zip alphabet newHist)   
+
+
+//printfn "random string of occurrence %A are %A" (cooccurrence storyContent) (fstOrderMarkovModel (cooccurrence storyContent) (storyContent.Length))
+
+printfn "charpair %A" ("""ab""")
