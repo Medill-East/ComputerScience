60e32979a6b71f1cdcb2250adf3125a9755d0241
gloomy-emerald <923770709@qq.com>
2019-07-11 17:57:13 +0800

    Create 7.6.fsx

---
+++ Professional Core Courses/Functional Programming/SummerSchool/SummerSchool/7.6.fsx
@@ -0,0 +1,67 @@
+﻿/// Reverse the order of a list of any type
+let rec reverse (lst : 'a list) : 'a list =
+  match lst with
+    elm :: rest -> (reverse rest) @ [elm]
+    | [] -> []
+
+/// Calculate the cumulative sum of a list of integers from the first to the last element. First element is the first number in the original list, last element is the sum of all integers in the original list.
+let cumSum (lst : int list) : int list =
+  /// Prepend the sum of the first element in acc and elm to acc.
+  let updateCumSum (acc : int list) (elm : int) : int list =
+    match acc with
+      [] -> [elm]
+      | a -> a.Head + elm :: a
+
+  List.fold updateCumSum [] lst |> reverse
+
+/// Given a monotonic function and an index into its value set, find the corresponding value on its definition set.
+let reverseLookup (monotonic : 'a list) (v : 'a) : int =
+    try
+      List.findIndex (fun w -> w > v) monotonic
+    with
+      _ -> monotonic.Length - 1
+
+/// Generate a random character according to a histogram.
+let rnd = System.Random() // A global object, if included in randomChar then seed point only changes slowly giving strongly correlated results in time
+let randomChar (hist : int list) : char =
+  let cumHist = cumSum hist
+  let v = rnd.Next(cumHist.[cumHist.Length-1])
+  let i = reverseLookup cumHist v
+  'a' + char i
+
+/// Generate a string of random characters each distributed according to a histogram.
+let randomString (hist : int list) (len : int) : string = 
+  /// Append random characters to a string, each distributed according to a histogram.
+  let rec appendRandomString (hist : int list) (len : int) (src : string) : string = 
+    if len = 0 then
+      src
+    else
+      src + string (randomChar hist) |> appendRandomString hist (len-1)
+
+  appendRandomString hist len ""
+let a = "LITTLECLAUSANDBIGCLAUSatranslationofhanschristianandersen’s’lilleclaus og store claus’ by jean hersholt. In a village there lived two men who had the self-same name. Both were named Claus. Butoneofthemownedfourhorses,andtheotherownedonlyonehorse;sotodistinguish between them people called the man who had four horses Big Claus, and the man who had only one horse Little Claus. Now I’ll tell you what happened to these two, for this is a true story."
+let rec countnum (src:string) (charpair:string):int =
+  if src.Length >=2 then
+    if src.[0..1] = charpair then
+      1+(countnum src.[1..] charpair)
+    else
+      (countnum src.[1..] charpair)
+  else
+    0
+let rec countcharpair (src:string) (letter1:char) (letter2:char): int list =
+  if letter2 <= 'z' then
+    let charpair = string letter1 + string letter2
+    (countnum src charpair) :: (countcharpair src letter1 (letter2 + char 1))
+  else
+    []
+
+let rec countchar (src:string) (letter1:char) : int list list = 
+  if letter1 <= 'z' then
+    (countcharpair src letter1 'a')::(countchar src (letter1 + char 1))
+  else
+    []
+
+let b = [4;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0]
+let randomStringAlt (hist: int list):string = randomString hist a.Length
+let c = randomStringAlt b 
+printfn "%A" c
