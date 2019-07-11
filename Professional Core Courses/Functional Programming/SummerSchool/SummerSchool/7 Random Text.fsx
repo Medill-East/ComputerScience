02f97594e8155b27483c7baeeeb14a36f1d983d6
gloomy-emerald <923770709@qq.com>
2019-07-11 13:02:31 +0800

    Update 7 Random Text.fsx
    201907111302

--- Professional Core Courses/Functional Programming/SummerSchool/SummerSchool/7 Random Text.fsx
+++ Professional Core Courses/Functional Programming/SummerSchool/SummerSchool/7 Random Text.fsx
@@ -27,3 +27,10 @@ let rec convertText (src:string) : string =
             
 printfn "convert %A to %A" testString (convertText testString)
 
+//7.3
+let rec countchar (src:string)(letter:char): int list = 
+  if letter >= 'a' && letter <= 'z' then 
+    ((String.filter (fun char -> char=letter) src).Length):: (countchar src (char (int letter + 1)))
+  else
+    []
+let histogram (src:string): int list = (countchar src 'a')
