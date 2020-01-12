6c835a07b37c856feccd2b62a02adbd5ce5ab4bf
Medill-East <NEMailLHD@163.com>
2020-01-12 16:55:15 +0800

    Algorithms 1.1.1 BinarySearch
    202001121655

---
+++ Professional Core Courses/Algorithms/Algorithms_in_CSharp/Algorithms_in_CSharp/Chap01/1.1.1 BinarySearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Algorithms_in_CSharp.Chap01
+{
+	public class BinarySearch
+	{
+		public BinarySearch()
+		{
+		}
+
+        public static object BinarySearchDisplay(int[] inputList, int target)
+        {
+            int minNum = 0;
+            int maxNum = inputList.Length - 1;
+
+            while(minNum<=maxNum)
+            {
+                int mid = (minNum + maxNum) / 2;
+                if(target == inputList[mid])
+                {
+                    return mid;
+                }
+                else if (target > inputList[mid])
+                {
+                    minNum = mid + 1;
+                }
+                else
+                {
+                    maxNum = mid - 1;
+                }
+            }
+            return "None";
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] inputList = { 7, 9, 5, 2, 3 };
+            Array.Sort(inputList);
+
+            Console.WriteLine(BinarySearchDisplay(inputList, 3));
+            Console.WriteLine(BinarySearchDisplay(inputList, 1));
+
+
+        }
+    }
+}
